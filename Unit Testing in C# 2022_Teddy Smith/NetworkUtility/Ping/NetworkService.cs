547b0b30601e8b3886e2ae00ff255f65aa696d10
ChangDaze <explision000@gmail.com>
2024-08-26 01:38:51 +0800

    [feature]Testing Objects, IEnumerable, & Dates

--- Unit Testing in C# 2022_Teddy Smith/NetworkUtility/Ping/NetworkService.cs
+++ Unit Testing in C# 2022_Teddy Smith/NetworkUtility/Ping/NetworkService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,10 +16,43 @@ namespace NetworkUtility.Ping
             //BuildPacket();
             return "Success: Ping Sent Success!";
         }
-
         public int PingTimeout(int a, int b)
         {
             return a + b;
+        }
+        public DateTime LastPingDate()
+        {
+            return DateTime.Now;
+        }
+        public PingOptions GetPingOptions()
+        {
+            return new PingOptions() //是.Net內建的型別，單純拿來測試用
+            {
+                DontFragment = true,
+                Ttl = 1 //設成0好像會錯， must be a non-negative and non-zero value...
+            };
+        }
+        public IEnumerable<PingOptions> MostRecentPings()
+        {
+            IEnumerable<PingOptions> pingOptions = new[]
+            {
+                new PingOptions()
+                {
+                    DontFragment = true,
+                    Ttl = 1
+                },
+                new PingOptions()
+                {
+                    DontFragment = true,
+                    Ttl = 1
+                },
+                new PingOptions()
+                {
+                    DontFragment = true,
+                    Ttl = 1
+                },
+            };
+            return pingOptions;
         }
     }
 }
--- Unit Testing in C# 2022_Teddy Smith/NetworkUtility/README.md
+++ Unit Testing in C# 2022_Teddy Smith/NetworkUtility/README.md
@@ -1,3 +1,4 @@
 reference : 
 https://www.youtube.com/watch?v=MJhQCMnRggs&list=PL82C6-O4XrHeyeJcI5xrywgpfbrqdkQd4&index=2
 https://fluentassertions.com/introduction
+https://www.youtube.com/watch?v=8UVrVynTZsg&list=PL82C6-O4XrHeyeJcI5xrywgpfbrqdkQd4&index=3
