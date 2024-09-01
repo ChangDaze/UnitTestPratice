398ec37554653f9a09b3a37d35f02a6953574a9b
ChangDaze <explision000@gmail.com>
2024-09-02 03:13:47 +0800

    [feature]Testing Objects, IEnumerable, & Dates, recommit test project

--- Unit Testing in C# 2022_Teddy Smith/NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
+++ Unit Testing in C# 2022_Teddy Smith/NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
@@ -1,8 +1,10 @@
 ﻿using FluentAssertions;
+using FluentAssertions.Extensions;
 using NetworkUtility.Ping;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,14 +12,20 @@ namespace NetworkUtility.Tests.PingTests
 {
     public class NetworkServiceTests
     {
+        private readonly NetworkService _networkService;
+        public NetworkServiceTests()
+        {
+            //SUT : System Under Test, 也能是此測試class下大家都會用到的物件?來讓程式碼簡潔
+            //Arrange - variables, classes, mocks            
+            _networkService = new NetworkService();
+        }
         [Fact] //Xunit 檢測是否要測試的屬性
         public void NetworkService_SendPing_ReturnString()
         {
             //Arrange - variables, classes, mocks
-            var pingService = new NetworkService();
 
             //Act
-            var result = pingService.SendPing();
+            var result = _networkService.SendPing();
 
             //Assert
             result.Should().NotBeNullOrWhiteSpace();
@@ -30,15 +38,62 @@ namespace NetworkUtility.Tests.PingTests
         public void NetworkService_PingTimeout_ReturnInt(int a, int b, int expected)
         {
             //Arrange - variables, classes, mocks
-            var pingService = new NetworkService();
 
             //Act
-            var result = pingService.PingTimeout(a, b);
-
+            var result = _networkService.PingTimeout(a, b);
+            
             //Assert
             result.Should().Be(expected);
             result.Should().BeGreaterThanOrEqualTo(2);
             result.Should().NotBeInRange(10000, 0);
+        }
+        [Fact]
+        public void NetworkService_LastPingDate_ReturnDate()
+        {
+            //Arrange - variables, classes, mocks
+
+            //Act
+            var result = _networkService.LastPingDate();
+
+            //Assert
+            result.Should().BeAfter(1.January(2010));
+            result.Should().BeBefore(1.January(2030));
+        }
+        [Fact]
+        public void NetworkService_GetPingOptions_ReturnObject()
+        {
+            //Arrange - variables, classes, mocks
+            var expected = new PingOptions
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+
+            //Act
+            var result = _networkService.GetPingOptions();
+
+            //Assert WARNING: "Be" careful ， 好像就是Be可以比對型別種類、型別相等、值等等各種方面來達成嚴格測試
+            result.Should().BeOfType<PingOptions>();
+            result.Should().BeEquivalentTo(expected); //BeEquivalentTo : have equally named properties with the same value...
+            result.Ttl.Should().Be(1);
+        }
+        [Fact]
+        public void NetworkService_MostRecentPings_ReturnObject()
+        {
+            //Arrange - variables, classes, mocks
+            var expected = new PingOptions
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+
+            //Act
+            var result = _networkService.MostRecentPings();
+
+            //Assert WARNING: "Be" careful ， 好像就是Be可以比對型別種類、型別相等、值等等各種方面來達成嚴格測試
+            result.Should().BeOfType<PingOptions[]>(); //這會用實作的類別來比，BeOfType可能比不了Interface?
+            result.Should().ContainEquivalentOf(expected);// at least contain one object equivalent to another object
+            result.Should().Contain(x => x.DontFragment == true);
         }
     }
 }
